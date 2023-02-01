9bb91774f5c2c043c841d2bc25cc0fe1376c304a
lixiang810 <14120445+lixiang810@users.noreply.github.com>
2023-02-01 13:43:49 +0800

    ✨ PV and comment

--- src/controllers/configProvider.ts
+++ src/controllers/configProvider.ts
@@ -1,5 +1,7 @@
 import type { WalineInitOptions, WalineProps } from '@waline/client';
-import { createMemo, createRoot, createSignal } from 'solid-js';
+import { createEffect, createMemo, createRoot, createSignal } from 'solid-js';
+import { commentCount } from '../waline/comment';
+import { pageviewCount } from '../waline/pageview';
 import { getConfig } from '../waline/utils/config';
 import { getRoot } from '../waline/utils/getRoot';
 
@@ -7,6 +9,9 @@ const configProvider = createRoot(() => {
   const [props, setProps] = createSignal<WalineProps>({ serverURL: '', path: '' });
   const config = createMemo(() => getConfig(props()));
   const locale = createMemo(() => config().locale);
+  const [pageView, setPageView] = createSignal<WalineInitOptions['pageview']>(undefined);
+  const [mountCommentCount, setMountCommentCount] =
+    createSignal<WalineInitOptions['comment']>(undefined);
   const init = ({
     el = '#sodesu',
     path = window.location.pathname,
@@ -17,11 +22,32 @@ const configProvider = createRoot(() => {
     if (!root) throw new Error('Cannot get root!');
     if (!initProps.serverURL) throw new Error("Option 'serverURL' is missing!");
     setProps({ ...initProps, path });
+    setPageView(initProps.pageview);
+    setMountCommentCount(initProps.comment);
     return root;
   };
   const update = (opts: Partial<Omit<WalineInitOptions, 'el'>>) => {
     setProps((p) => ({ ...p, opts }));
   };
+  createEffect(() => {
+    if (pageView()) {
+      pageviewCount({
+        serverURL: config().serverURL,
+        path: config().path,
+        selector: typeof pageView() === 'string' ? (pageView() as string) : undefined,
+      });
+    }
+  });
+  createEffect(() => {
+    if (mountCommentCount()) {
+      commentCount({
+        serverURL: config().serverURL,
+        path: config().path,
+        selector:
+          typeof mountCommentCount() === 'string' ? (mountCommentCount() as string) : undefined,
+      });
+    }
+  });
   return { setProps, config, locale, init, update };
 });
 
