7489032e78a7052441facf004ed0b797a165abb2
BeiyanYunyi <14120445+BeiyanYunyi@users.noreply.github.com>
2023-02-11 22:47:57 +0800

    💄 Add style for code

--- docs/.vitepress/theme/SodesuLayout.vue
+++ docs/.vitepress/theme/SodesuLayout.vue
@@ -10,6 +10,16 @@
     </template>
     <template #layout-bottom>
       <ReloadPrompt />
+      <link
+        v-if="isDark"
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/prismjs@1.29.0/themes/prism-dark.min.css"
+      />
+      <link
+        v-else
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/prismjs@1.29.0/themes/prism.min.css"
+      />
     </template>
   </Layout>
 </template>
@@ -17,7 +27,9 @@
 import DefaultTheme from 'vitepress/theme';
 import SodesuComment from './SodesuComment.vue';
 import ReloadPrompt from './ReloadPrompt.vue';
+import { useData } from 'vitepress';
 
+const { isDark } = useData();
 const { Layout } = DefaultTheme;
 </script>
 <style scoped>
