9e003474835f0a8349d78fc701d1b7de6a67cdf3
BeiyanYunyi <14120445+BeiyanYunyi@users.noreply.github.com>
2023-05-27 16:06:19 +0800

    🐛 Fix like action

--- src/components/CommentCardActions.tsx
+++ src/components/CommentCardActions.tsx
@@ -2,7 +2,7 @@ import { Component, createMemo, Show } from 'solid-js';
 import commentBoxState, { clearReplyState } from '../controllers/commentBoxState';
 import { ReactiveComment } from '../controllers/commentListState';
 import configProvider from '../controllers/configProvider';
-import { handleLike } from '../controllers/likeState';
+import likeState, { handleLike } from '../controllers/likeState';
 import userInfoState from '../controllers/userInfoState';
 import { EditIcon, LikeIcon, ReplyIcon } from './Icons';
 
@@ -10,6 +10,8 @@ const CommentCardActions: Component<{ comment: ReactiveComment; rootId: string }
   const { locale } = configProvider;
   const { replyId, setReplyId, setReplyUser, setRootId, setContent, setEdit, edit } =
     commentBoxState;
+  const { likes } = likeState;
+  const liked = createMemo(() => likes()?.includes(props.comment.objectId) || false);
   const isReplyingCurrent = createMemo(() => replyId() === props.comment.objectId);
   const { userInfo, isAdmin } = userInfoState;
   const isOwner = createMemo(() =>
@@ -39,13 +41,13 @@ const CommentCardActions: Component<{ comment: ReactiveComment; rootId: string }
       <button
         type="button"
         class="inline-flex items-center border-none bg-transparent text-sColor hover:text-sActive transition-colors cursor-pointer sds-btn me-2"
-        title={props.comment.like() ? locale().cancelLike : locale().like}
+        title={liked() ? locale().cancelLike : locale().like}
         onClick={(e) => {
           e.preventDefault();
           handleLike(props.comment);
         }}
       >
-        <LikeIcon active={!!props.comment.like()} />
+        <LikeIcon active={liked()} />
         <span>{props.comment.like()}</span>
       </button>
       <Show when={!editingThis()}>
