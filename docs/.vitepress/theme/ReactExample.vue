1683507fd77b26c58fb5672c6f120707365e6ac0
BeiyanYunyi <14120445+BeiyanYunyi@users.noreply.github.com>
2024-12-24 13:32:10 +0800

    docs: :memo: react example

---
+++ docs/.vitepress/theme/ReactExample.vue
@@ -0,0 +1,31 @@
+<template>
+  <div ref="exampleContainer" />
+</template>
+<script setup lang="ts">
+import type { VM } from '@stackblitz/sdk';
+import type { VNodeRef } from 'vue';
+import sdk from '@stackblitz/sdk';
+import { onMounted, onUnmounted, ref } from 'vue';
+
+const exampleContainer = ref<HTMLDivElement>();
+const vm = ref<VM>();
+
+onMounted(() =>
+  sdk
+    .embedProjectId(exampleContainer.value!, 'sodesu-react-example', {
+      forceEmbedLayout: true,
+      openFile: 'src/components/Comments.tsx',
+      clickToLoad: true,
+      hideExplorer: true,
+      height: 600,
+      view: 'preview',
+    })
+    .then((value) => {
+      vm.value = value;
+    }),
+);
+
+onUnmounted(() => {
+  exampleContainer.value && (exampleContainer.value.innerHTML = '');
+});
+</script>
