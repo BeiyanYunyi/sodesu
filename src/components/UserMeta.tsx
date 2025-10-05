554c8d809f9c39ae8158a1de9d902d30a7f27432
BeiyanYunyi <14120445+BeiyanYunyi@users.noreply.github.com>
2025-10-05 11:19:28 +0800

    fix(UserMeta): :bug: fix border-bottom, sync with waline

--- src/components/UserMeta.tsx
+++ src/components/UserMeta.tsx
@@ -1,5 +1,5 @@
-import type {Component} from 'solid-js';
-import {  createMemo, For } from 'solid-js';
+import type { Component } from 'solid-js';
+import { createMemo, For } from 'solid-js';
 import { userMetaState } from '../controllers/commentBoxState';
 import configProvider from '../controllers/configProvider';
 
@@ -7,12 +7,12 @@ const UserMeta: Component = () => {
   const { config, locale } = configProvider;
   const { userMeta, setUserMeta, inputRefs } = userMetaState;
   return (
-    <div class="bb flex flex-wrap justify-evenly px-4 text-[0.625rem]">
+    <div class="flex flex-wrap justify-evenly px-4 text-[0.75rem] bb-2">
       <For each={config().meta}>
         {(kind) => {
           const value = createMemo(() => userMeta()[kind]);
           return (
-            <div class="flex flex-grow items-center">
+            <div class="flex flex-grow items-stretch">
               <label for={`wl-${kind}`} class="px-2 py-3 text-sColor">
                 {locale()[kind] +
                   (config().requiredMeta.includes(kind) || !config().requiredMeta.length
